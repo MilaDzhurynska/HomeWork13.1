ea88c8e3cdfb34c0d8ea2b42720edd9cdbcd9f75
DZHURYNSKA LIUDMYLA <hello.its.me.luda@gmail.com>
2025-03-09 19:15:38 +0200

    first commit

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square implements Shape{
+    private double side;
+    public Square(double side){
+        this.side = side;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.pow(side,2);
+    }
+}
